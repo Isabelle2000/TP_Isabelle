b43349550bc3e6b1dd51d2435215e2e614c32d2d
Isabelle2000 <62118894+Isabelle2000@users.noreply.github.com>
2020-03-13 14:04:11 -0400

    création de api.py

---
+++ api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+url_base = 'https://python.gel.ulaval.ca/quoridor/api'
+
+rep = requests.get('{}/lister/'.format(url_base), params={'idul': 'islev54'})
+
+if rep.status_code == 200:
+    rep = rep.json()
+    print(rep)    
+else:
+    print("Le GET sur '{}lister' a produit le code d'erreur {}.".format(
+        url_base, rep.status_code)
+        )
