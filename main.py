9507f95d81799201a3aeb1a788ef1c81463a1500
Isabelle2000 <62118894+Isabelle2000@users.noreply.github.com>
2020-03-16 21:06:22 -0400

    modifications

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import api
 def analyser_commande():
     parser = argparse.ArgumentParser(description='Jeu Quoridor - phase 1')
     parser.add_argument('idul', metavar='IDUL', 
-                        default='islev54', help="IDUL du joueur")
-    parser.add_argument('-l', '--lister', dest='liste',
-                        help='Lister les identifiants de vos 20 dernières parties.')
+                        default='islev54', help="IDUL du joueur.")
+    parser.add_argument('-l', '--lister', help='Lister les identifiants de vos 20 dernières parties.')
     return parser.parse_args()
+
 
 def afficher_damier_ascii(etat_de_jeu):
     damier, verticale, horizontale = [], [], []
