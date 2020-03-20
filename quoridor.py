ecdb1e8583ce46576eb0681c9670994356c160cb
Isabelle2000 <62118894+Isabelle2000@users.noreply.github.com>
2020-03-19 21:40:58 -0400

    enlever le 'liste' qui s'affichait en trop en modifiant parser.add

--- quoridor.py
+++ quoridor.py
@@ -6,7 +6,8 @@ def analyser_commande():
     parser = argparse.ArgumentParser(description='Jeu Quoridor - phase 1')
     parser.add_argument('idul', metavar='IDUL',
                         default='islev54', help="IDUL du joueur.")
-    parser.add_argument('-l', '--lister', dest='liste', help='Lister les identifiants de vos 20 dernières parties.')
+    parser.add_argument('-l', '--lister', action='store_true',
+                        help='Lister les identifiants de vos 20 dernières parties.')
     return parser.parse_args()
 
 
